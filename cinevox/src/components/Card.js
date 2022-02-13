a8483e2aa7f267ff42a5e12bad1beb2dd4a0e190
Webme <guillaume.gackel@gmail.com>
2022-02-13 22:04:33 +0100

    sort et search ok

--- cinevox/src/components/Card.js
+++ cinevox/src/components/Card.js
@@ -1,13 +1,121 @@
 import React from 'react';
 
 const Card = ({movie}) => {
+const dateFormatter=(date)=>{
+	let[yy, mm, dd]=date.split("-")
+	// return dd+" "+mm+" "+" "+yy
+return[dd,mm,yy].join("/")};
+
+const genreFinder =()=>{
+let genreArray =[];
+
+for (let i = 0; i<movie.genre_ids.length; i++){
+	switch(movie.genre_ids[i]){
+
+		case 28:
+		genreArray.push('Action');
+		break;
+	
+		case 12:
+		genreArray.push("Aventure");
+		break;
+	
+		case 16:
+		genreArray.push("Animation");
+		break;
+
+		case 35:
+		genreArray.push("Comédie");
+		break;
+
+		case 80:
+		genreArray.push("Crime");
+		break;
+
+		case 99:
+		genreArray.push("Documentaire");
+		break;
+
+		case 18:
+		genreArray.push("Drame");
+		break;
+
+		case 10751:
+		genreArray.push("Familial");
+		break;
+
+
+		case 14:
+		genreArray.push("Fantastique");
+		break;
+
+		case 36:
+		genreArray.push("Histoire");
+		break;
+
+		case 27:
+		genreArray.push("Horreur");
+		break;
+
+		case 10402:
+		genreArray.push("Musique");
+		break;
+
+		case 9648:
+		genreArray.push("Mystère");
+		break;
+
+		case 10749:
+		genreArray.push("Romance");
+		break;
+
+		case 878:
+		genreArray.push("Science-Fiction");
+		break;
+
+		case 10770:
+		genreArray.push("Téléfilm");
+		break;
+
+		case 53:
+		genreArray.push("Thriller");
+		break;
+
+
+		case 10752:
+		genreArray.push("Guerre");
+		break;
+
+
+		case 37:
+		genreArray.push("Western");
+		break;
+		default:
+			break;
+
+	}
+}
+return genreArray.map((genre) => <li key={genre}>{genre}</li>)
+};
+
 	return (
 		<div>
 			<div className="card">
 				 <img src= {movie.poster_path ?
 					"https://images.tmdb.org/t/p/w500" + movie.poster_path : "./img/poster.jpg"} alt=''/>
 				<h2>{movie.title}</h2>
+				{movie.release_date ?(
+					<h5>sorti le: {dateFormatter(movie.release_date)}</h5>
+				) : "pas précisé"}
+				<h4>{movie.vote_average}/10 <span>⭐</span></h4>
+				<ul>{genreFinder()}</ul>
+				{movie.overview ? <h3>Synoppsis</h3> :"non renseigné"}
+				<p>{movie.overview}</p>
+
+				<div className="btn">Ajouter aux coups de coeur</div>
+
 			</div>
+
 		</div>
 	);
 };
--- cinevox/src/components/Form.js
+++ cinevox/src/components/Form.js
@@ -4,38 +4,63 @@ import Card from "./Card";
 
 const Form = () => {
   const [moviesdata, setMoviesdata] = useState([]);
+  const [search, setsearch] = useState("null");
+  const [sortGoodBad, setSortGoodBad] = useState("null");
 
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/search/movie?api_key=d6550bd46ed439905916ceb920fb07c4&query=avenger&language=fr-FR"
+        `https://api.themoviedb.org/3/search/movie?api_key=d6550bd46ed439905916ceb920fb07c4&query=${search}&language=fr-FR`
       )
 
       .then((res) => setMoviesdata(res.data.results));
-  }, []);
+  }, [search]);
 
   return (
     <div className="form-component">
       <div className="form-container">
         <form action="">
-          <input type="text" placeholder="Entrez le titre" id="search-input" />
+          <input
+            type="text"
+            placeholder="Entrez le titre"
+            id="search-input"
+            onChange={(e) => setsearch(e.target.value)}
+          />
+
           <input type="submit" value="Recherchez" />
         </form>
 
         <div className="btn-sort-container">
-          <div className="bt-sort" id="goodToBad">
+          <div
+            className="bt-sort"
+            id="goodToBad"
+            onClick={() => setSortGoodBad("goodToBad")}
+          >
             Top<span>--></span>
           </div>
-          <div className="bt-sort" id="badToGood">
+          <div
+            className="bt-sort"
+            id="badToGood"
+            onClick={() => setSortGoodBad("badToGood")}
+          >
             Flop<span>--></span>
           </div>
         </div>
       </div>
 
       <div className="result">
-        {moviesdata.slice(0,12).map((movie) => (
-          <Card  key={movie.id} movie={movie}/>
-        ))}
+        {moviesdata
+          .slice(0, 12)
+          .sort((a, b) => {
+            if (sortGoodBad === "goodToBad") {
+              return b.vote_average - a.vote_average;
+            } else if (sortGoodBad === "badToGood") {
+              return a.vote_average - b.vote_average;
+            }
+          })
+          .map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
       </div>
     </div>
   );
